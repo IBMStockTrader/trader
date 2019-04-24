7e42719958b770f70322ebe6f3e7f8b7c8bd4c3d
John Alcorn <jalcorn@us.ibm.com>
2019-04-23 17:47:22 -0700

    JSON-B POJO for a Portfolio

---
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/trader/Portfolio.java
@@ -0,0 +1,172 @@
+/*
+       Copyright 2017-2019 IBM Corp All Rights Reserved
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package com.ibm.hybrid.cloud.sample.stocktrader.trader.json;
+
+import java.util.Iterator;
+import java.util.Set;
+
+//JSON-P 1.0 (JSR 353).  This replaces my old usage of IBM's JSON4J (com.ibm.json.java.JSONObject)
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
+
+/** JSON-B POJO class representing a Portfolio JSON object */
+public class Portfolio {
+    private String owner;
+    private double total;
+    private String loyalty;
+    private double balance;
+    private double commissions;
+    private int free;
+    private String sentiment;
+    private double nextCommission;
+    private JsonObject stocks;
+
+
+    public Portfolio() { //default constructor
+    }
+
+    public Portfolio(String initialOwner) { //primary key constructor
+        setOwner(initialOwner);
+    }
+
+    public Portfolio(String initialOwner, double initialTotal, String initialLoyalty, double initialBalance,
+                     double initialCommissions, int initialFree, String initialSentiment, double initialNextCommission) {
+        setOwner(initialOwner);
+        setTotal(initialTotal);
+        setLoyalty(initialLoyalty);
+        setBalance(initialBalance);
+        setCommissions(initialCommissions);
+        setFree(initialFree);
+        setSentiment(initialSentiment);
+        setNextCommission(initialNextCommission);
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String newOwner) {
+        owner = newOwner;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double newTotal) {
+        total = newTotal;
+    }
+
+    public String getLoyalty() {
+        return loyalty;
+    }
+
+    public void setLoyalty(String newLoyalty) {
+        loyalty = newLoyalty;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double newBalance) {
+        balance = newBalance;
+    }
+
+    public double getCommissions() {
+        return commissions;
+    }
+
+    public void setCommissions(double newCommissions) {
+        commissions = newCommissions;
+    }
+
+    public int getFree() {
+        return free;
+    }
+
+    public void setFree(int newFree) {
+        free = newFree;
+    }
+
+    public String getSentiment() {
+        return sentiment;
+    }
+
+    public void setSentiment(String newSentiment) {
+        sentiment = newSentiment;
+    }
+
+    public double getNextCommission() {
+        return nextCommission;
+    }
+
+    public void setNextCommission(double newNextCommission) {
+        nextCommission = newNextCommission;
+    }
+
+    public JsonObject getStocks() {
+        return stocks;
+    }
+
+    public void addStock(Stock newStock) {
+        if (newStock != null) {
+            String symbol = newStock.getSymbol();
+            if (symbol != null) {
+                JsonObjectBuilder stocksBuilder = Json.createObjectBuilder();
+            
+                if (stocks != null) { //JsonObject is immutable, so copy current "stocks" into new builder
+                    Iterator<String> iter = stocks.keySet().iterator();
+                    while (iter.hasNext()) {
+                        String key = iter.next();
+                        JsonObject obj = stocks.getJsonObject(key);
+                        stocksBuilder.add(key, obj);
+                    }
+                }
+
+                //can only add a JSON-P object to a JSON-P object; can't add a JSON-B object.  So converting...
+                JsonObjectBuilder builder = Json.createObjectBuilder();
+
+                builder.add("symbol", symbol);
+                builder.add("shares", newStock.getShares());
+                builder.add("commission", newStock.getCommission());
+                builder.add("price", newStock.getPrice());
+                builder.add("total", newStock.getTotal());
+                builder.add("date", newStock.getDate());
+
+                JsonObject stock = builder.build();
+
+                stocksBuilder.add(symbol, stock); //might be replacing an item; caller needs to do any merge (like updatePortfolio does)
+                stocks = stocksBuilder.build();
+            }
+        }
+    }
+
+    public boolean equals(Object obj) {
+        boolean isEqual = false;
+        if ((obj != null) && (obj instanceof Portfolio)) isEqual = toString().equals(obj.toString());
+        return isEqual;
+   }
+
+    public String toString() {
+        return "{\"owner\": \""+owner+"\", \"total\": "+total+", \"loyalty\": \""+loyalty+"\", \"balance\": "+balance
+               +", \"commissions\": "+commissions+", \"free\": "+free+", \"nextCommission\": "+nextCommission
+               +", \"sentiment\": \""+sentiment+"\", \"stocks\": "+(stocks!=null?stocks.toString():"{}")+"}";
+    }
+}
